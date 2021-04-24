41b3ab672bc25158bc1559718abff722ea64a7c5
Najmul Huda <najmul.myself@gmail.com>
2021-04-25 02:58:32 +0600

    btn and link routing

--- src/Component/User/User.js
+++ src/Component/User/User.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 const User = (props) => {
     const {name,email,id} = props.eachUser;
@@ -10,12 +10,19 @@ const User = (props) => {
         width : '500px',
         display : 'inline-flex',
     }    
+    // To use onclick React Router has built in hook call useHistory.we need to call it
+    const history = useHistory()
+    // then we need to add event handlers ,it will want a parameter so that it can dynamically changes the key.
+    const handleClick = userId => history.push(`/user/${userId}`);
+    // history.push is useHistory() hooks statement. more about useHistory in React Router official documentation.
     return (
         <div style={userStyles}>
             <div>
             <h1>{name}</h1>
             <p><small>{email}</small></p>
-            <button><Link to={`user/${id}`}>See Details</Link></button>
+            <Link to={`user/${id}`}>See Details</Link>
+            {/* handleClick function wil need to pass an argument which is an id. */}
+            <button onClick={() => handleClick(id)}>Click to see </button>
             </div>
         </div>
     );
